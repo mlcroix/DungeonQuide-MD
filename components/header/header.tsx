46d596e8d73161c306888f4a0faad5961992bacc
mlcroix <michelducroix@gmail.com>
2026-07-22 09:27:00 +0200

    Read out cookie in front-end - read cookie - check online status

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import Link from 'next/link';
 import './header.scss';
 import Logo from '@/components/logo';
 import PageNavigation from '../page-navigation/page-navigation';
+import cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [userData, setUserData] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    setUserData(cookies.get('user_data'));
+  }, []);
+
   const mainNavItems = [
     { label: 'News', href: '/news' },
     { label: 'Campaigns', href: '/campaigns' },
@@ -11,11 +21,16 @@ export default function Header() {
     { label: 'Community', href: '/community' }
   ];
   
-  const userNavItems = [
-    { label: 'Profile', href: '/profile' },
-    { label: 'Settings', href: '/settings' },
-    { label: 'Logout', href: '/logout' }
-  ];
+  const userNavItems = userData
+    ? [
+        { label: 'Profile', href: '/profile' },
+        { label: 'Settings', href: '/settings' },
+        { label: 'Logout', href: '/logout' }
+      ]
+    : [
+        { label: 'Login', href: '/login' },
+        { label: 'Sign up', href: '/signup' }
+      ];
 
   return (
     <div className='header'>
